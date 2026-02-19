1351475b9a20e4dbe7b5a39a0b2b630a83ea41e6
Sumit Kumar <tbeast935@gmail.com>
2026-02-19 09:04:07 +0530

    op

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title'
 
 const Orders = () => {
 
   const {product,currency} = useContext(ShopContext)
   return (
     <div className='border-t pt-16'>
+      <div className="text-2xl">
+        <Title text1={'MY'} text2={'ORDERS'}
+      </div>
       
     </div>
   )
