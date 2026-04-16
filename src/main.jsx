4af1da8417293bce76b8ce2ed85d48dae2f9ec0c
Sumit Kumar <tbeast935@gmail.com>
2026-04-16 23:06:19 +0530

    ui

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
+
 createRoot(document.getElementById("root")).render(
   
  <BrowserRouter>
